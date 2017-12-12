7fcefe346a020a4158b9e0d7ec94c4a28afe88ad
Guido <guidothomasse@hotmail.com>
2017-12-12 12:51:30 +0100

    fixed texture so that the bak is in overeenkomst with the kleuren pickups

--- E-Waste Arcade Game/GameObjects/Bin.ts
+++ E-Waste Arcade Game/GameObjects/Bin.ts
@@ -4,7 +4,7 @@
         collectWasteTypeState: WasteType;
 
         constructor(game: Phaser.Game, x: number, y: number) {
-            super(game, x, y, "bin1");
+            super(game, x, y, "bin3");
             this.game = game;
             this.collectWasteTypeState = WasteType.WASTE_1
         }
@@ -13,17 +13,17 @@
             switch (this.collectWasteTypeState) {
                 case WasteType.WASTE_1: {
                     this.collectWasteTypeState = WasteType.WASTE_2;
-                    this.loadTexture("bin2");
+                    this.loadTexture("bin3");
                     break;
                 }
                 case WasteType.WASTE_2: {
                     this.collectWasteTypeState = WasteType.WASTE_3;
-                    this.loadTexture("bin3");
+                    this.loadTexture("bin1");
                     break;
                 }
                 case WasteType.WASTE_3: {
                     this.collectWasteTypeState = WasteType.WASTE_1;
-                    this.loadTexture("bin1");
+                    this.loadTexture("bin2");
                     break;
                 }
             }
