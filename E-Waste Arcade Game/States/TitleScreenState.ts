85d2c325970502d5ab3c6917f0d1c72d729a45db
Joël <j.debont94@hotmail.com>
2017-12-19 11:00:58 +0100

    changed the title screen. Waiting for visual decisions to finish it

--- E-Waste Arcade Game/States/TitleScreenState.ts
+++ E-Waste Arcade Game/States/TitleScreenState.ts
@@ -2,11 +2,24 @@
     export class TitleScreenState extends Phaser.State {
         game: Phaser.Game;
         titleScreenImage: Phaser.Sprite;
+        joystickJump: Phaser.Sprite;
+        buttonSwitchLeft: Phaser.Sprite;
+        buttonSwitchRight: Phaser.Sprite;
+        startGameSprite: Phaser.Sprite;
+        tweenStartGame: Phaser.Tween;
+        playerJump: Phaser.Sprite;
 
         YELLOW: Phaser.Key;
         
         create() {
             this.titleScreenImage = this.add.sprite(0, 0, "title");
+
+            this.playerJump = this.add.sprite(250, 50, "CHAR_JUMPING");
+
+            this.joystickJump = this.add.sprite(275, 200, "joystickJump");
+            this.buttonSwitchLeft = this.add.sprite(450, 275, "buttonSwitchLeft");
+            this.buttonSwitchRight = this.add.sprite(600, 275, "buttonSwitchRight");
+            this.startGameSprite = this.add.sprite(450, 425, "startGame");
 
             this.input.onTap.add(this.startGame, this);
 
@@ -16,6 +29,12 @@
             this.input.onDown.add(() => {
                 this.startGame();
             });
+            
+            this.tweenStartGame = this.game.add.tween(this.startGameSprite).to({ alpha: 0 }, 500, "Linear", true, 0, -1);
+            this.tweenStartGame.yoyo(true, 1);
+
+            this.playerJump.animations.add("jumping");
+            this.playerJump.animations.play("jumping", 10, true);
         }
 
         startGame() {
